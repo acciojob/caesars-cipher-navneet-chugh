e1d797c2fd09daffa359cedb0fbe3047088d6fcc
navneet-chugh <navneetchugh15@gmail.com>
2023-08-11 23:28:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Your Script here.
-
 const lookup = {
   A: "N",
   B: "O",
@@ -34,8 +33,13 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
+	for (let i = 0; i < encodedStr.length; i++) {
+    const char = encodedStr[i];
+    const decodedChar = lookup[char] || char; // Use the lookup table or keep unchanged
+    
+    decodedArr.push(decodedChar);
 
-  return; //return decodedArr
+  return decodedArr.join(''); //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
